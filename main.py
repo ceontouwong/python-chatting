2fe00671bda00257cc83b6a91e325ab3a0e582a0
ceontouwong <ceontouwong@outlook.com>
2018-09-14 22:18:08 +0800

    增加尚不完善的聊天机器人 -

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_socketio import SocketIO
+from chatbot.bot import *
 import time
+
+bot = Yuki()
 app = Flask(__name__)
 async_mode = None
 app.config['SECRET_KEY'] = "TestForFirstTime"
 socketio = SocketIO(app)
 
-@app.route('/', methods = ['GET', 'POST'])
+#首页
+@app.route('/')
+def index():
+    return redirect(url_for(chatbox))
+
+@app.route('/yukibot/')
 def chatbox():
     Talkto = "Yuki"
     return render_template('chatbox.html', name = Talkto)
@@ -15,29 +23,12 @@ def chatbox():
 def Test_Connect():
     print('CONNECTED!')
     socketio.emit('response', "CONNECTED TO SERVER SUCCESSFULLY!")
-    running()
 
 @socketio.on('send')
 def resp_send(msg):
-    socketio.emit('ReToSend', "I know that.")
-
-def sendback(msg):
-    socketio.emit('resp', msg)
-
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-
-def dealWithMsg(msg):
-    msg = "I have heard what you said:" + msg
-    return msg
-
-def running():
-    for i in range(5):
-        time.sleep(1)
-        sendback("emmm...")
+    response = bot.getResponse(msg)
+    socketio.emit('resp', response)
 
 if __name__ == '__main__':
     app.debug = True
     socketio.run(app)
-    running()
